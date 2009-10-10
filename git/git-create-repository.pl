d7045fe6f62eb17feb13e3e24a7770259546db75
wakaba <wakaba@melon.(none)>
2009-10-10 15:31:05 +0900

    moved functions to submodule; style fixes

--- git/git-create-repository.pl
+++ git/git-create-repository.pl
@@ -3,39 +3,13 @@ use strict;
 use warnings;
 use Path::Class;
 use File::Temp qw/tempdir/;
-
-sub x (@) {
-  print STDERR join ' ', @_;
-  print STDERR "\n";
-  system @_;
-} # x
-
-sub Path::Class::Dir::v_chdir ($) {
-  my $self = shift;
-  printf STDERR "cd %s\n", $self->stringify;
-  return chdir $self;
-} # v_chdir
-
-sub Path::Class::Dir::v_mkpath ($) {
-  my $self = shift;
-  printf STDERR "mkdir %s\n", $self->stringify;
-  return $self->mkpath;
-} # v_mkpath
-
-sub Path::Class::Entity::is_special_file_name ($) {
-  my $self = shift;
-  my $bn = $self->can('basename') ? 'basename' : undef;
-  if (defined $bn) {
-    return ($bn eq '.' or $bn eq '..');
-  } else {
-    my $name = $self->stringify;
-    return ($name =~ m[/\.\.?\z] or $name eq '.' or $name eq '..');
-  }
-} # is_special_file_name
+use lib file (__FILE__)->dir->parent->subdir ('modules')->subdir ('cmdutils')->subdir ('lib')->stringify;
+use Command qw/x/;
+use Extras::Path::Class;
 
 my $repos_d;
-my $template_d = file(__FILE__)->dir->subdir('template')->absolute;
-my $tmp_d = dir(tempdir)->absolute;
+my $template_d = file (__FILE__)->dir->subdir ('template')->absolute;
+my $tmp_d = dir (tempdir)->absolute;
 
 my $repo_category = shift;
 my $repo_name = shift;
@@ -44,14 +18,14 @@ die "Usage: perl $0 repository-category repository-name\n"
     unless $repo_category and $repo_name;
 
 if ($repo_category =~ m[\A(?:pub|melon)(?:/[\w-]+)?\z]) {
-  $repos_d = dir('/git')->absolute;
+  $repos_d = dir ('/git')->absolute;
 } elsif ($repo_category =~ m[\Atest(?:/[\w-]+)?\z]) {
-  $repos_d = dir('/tmp')->absolute;
+  $repos_d = dir ('/tmp')->absolute;
 } else {
   die "$0: Category $repo_category is not defined\n";
 }
 
-my $repo_d = $repos_d->subdir($repo_category)->subdir("$repo_name.git");
+my $repo_d = $repos_d->subdir ($repo_category)->subdir ("$repo_name.git");
 if (-d $repo_d) {
   die "$0: $repo_d: There is already a directory\n";
 }
@@ -63,7 +37,7 @@ x qw/git init --bare/;
 $tmp_d->v_chdir;
 x qw/git clone/, $repo_d, $repo_name;
 
-my $tmp_copy_d = $tmp_d->subdir($repo_name);
+my $tmp_copy_d = $tmp_d->subdir ($repo_name);
 $tmp_copy_d->v_chdir;
 
 while (my $f = $template_d->next) {
