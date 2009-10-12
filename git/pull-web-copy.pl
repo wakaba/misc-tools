56f890d33050938d4e17b0588ce7ff03739df951
Wakaba <w@suika.fam.cx>
2009-10-12 12:55:01 +0900

    New

---
+++ git/pull-web-copy.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Path::Class;
+use lib file (__FILE__)->dir->parent->subdir ('modules')->subdir ('cmdutils')->subdir ('lib')->stringify;
+use Command qw/x/;
+use Extras::Path::Class;
+
+my $git_d = dir (shift);
+
+warn "Updating $git_d...\n";
+
+$git_d->v_chdir;
+
+$ENV{GIT_DIR} = '.git';
+x qw/git pull/;
+
+warn "\n";
