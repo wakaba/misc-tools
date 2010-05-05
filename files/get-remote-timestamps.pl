5f8cbcb69aa85f6f2a6889b521f9120dbce7169b
Wakaba <w@suika.fam.cx>
2010-05-06 06:51:46 +0900

    new

---
+++ files/get-remote-timestamps.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Path::Class;
+use lib file (__FILE__)->dir->parent->subdir ('modules')->subdir ('cmdutils')->subdir ('lib')->stringify;
+use Command qw/x/;
+
+my $remote_path = q[/home/httpd/html/admin];
+my $remote_host = q[suika];
+my $exclude_pattern = qr[(?>^|/)(?>CVS|\.\.?|.*~)(?>$|/)];
+
+my $command = join ' ', map { quotemeta }
+    q[ssh], $remote_host,
+    map { my $a = $_; $a =~ s/\n/ /g; quotemeta $a }
+        qw{perl -MPath::Class -e},
+        q{
+          dir($here = shift)->recurse(callback => sub {
+            return unless -f $_[0];
+            print STDOUT $_[0]->stat->mtime, "\t", $_[0]->relative($here), "\n";
+          });
+        },
+        $remote_path;
+
+my %list;
+for (split /\n/, `$command`) {
+  my ($mtime, $path) = split /\s+/, $_;
+  next if $path =~ /$exclude_pattern/o;
+  $list{$path} = $mtime;
+}
+
+for (sort { $a cmp $b } keys %list) {
+  printf "%s\t%s\n", $list{$_}, $_;
+}
