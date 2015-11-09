e1c82bc43f7dad3a049b9921a943a90a5c33c447
Wakaba <wakaba@suikawiki.org>
2015-11-09 16:48:51 +0900

    test

--- config/python/setup.py
+++ config/python/setup.py
@@ -1,4 +1,4 @@
-import subprocess;
 from setuptools import setup;
-subprocess.call (["make", "pmbp-install"]);
+#import subprocess;
+#subprocess.call (["make", "pmbp-install"]);
 setup (name='misc',version='1.0.0');
