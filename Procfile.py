d693de4c26249d390af85b6feb4f7448f31c2cb5
Wakaba <wakaba@suikawiki.org>
2015-11-09 17:46:05 +0900

    deps

--- Procfile.py
+++ Procfile.py
@@ -1,1 +1,1 @@
-web: perl local/bin/pmbp.pl --install-module AnyEvent && ./plackup -s Twiggy::Prefork -p $PORT bin/server.psgi
+web: make pmbp-install && ./plackup -s Twiggy::Prefork -p $PORT bin/server.psgi
